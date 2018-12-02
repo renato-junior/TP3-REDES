b761f4d3c4892b8298fc4b9acf8f7da3c4da1008
Renato Júnior <renatosergiolopez@hotmail.com>
2018-12-02 16:14:53 -0200

    Implementa leitura dos arquivos json no servidor

--- src/server.py
+++ src/server.py
@@ -1,21 +1,29 @@
 import sys
+import json
 from flask import Flask
+
+
+class Server:
+    def __init__(self, netfile_name, ixfile_name, netixlanfile_name):
+        # Load data from json files
+        self.net_data = self.load_data_from_json(netfile_name)
+        self.ix_data = self.load_data_from_json(ixfile_name)
+        self.netixlan_data = self.load_data_from_json(netixlanfile_name)
+
+    def load_data_from_json(self, filename):
+        file_content = open(filename).read()
+        return json.loads(file_content)
+
 
 app = Flask(__name__)
 
+server = Server(
+    netfile_name=str(sys.argv[2]),
+    ixfile_name=str(sys.argv[3]),
+    netixlanfile_name=str(sys.argv[4]))
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return str(server.net_data)
 
-def main():
-    port = int(sys.argv[1])
-    # netfile = str(sys.argv[2])
-    # ixfile = str(sys.argv[3])
-    # netixlanfile = str(sys.argv[4])
-
-    app.run(host='127.0.0.1', port=port)
-
-
-
-if __name__ == '__main__':
-    main()
+app.run(host='127.0.0.1', port=int(sys.argv[1]))
